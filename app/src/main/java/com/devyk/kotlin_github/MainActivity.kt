ce5195de40206c346ef3c176f48ce0036ce978fa
yangkun19921001 <yang1001yk@gmail.com>
2019-10-29 15:18:40 +0800

    first init

---
+++ app/src/main/java/com/devyk/kotlin_github/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.devyk.kotlin_github
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
