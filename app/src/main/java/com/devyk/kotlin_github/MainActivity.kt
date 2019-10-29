2509c1b993a17f0b8e22b1ee689847340325ea31
yangkun19921001 <yang1001yk@gmail.com>
2019-10-29 15:33:03 +0800

    1.0.0.2

--- app/src/main/java/com/devyk/kotlin_github/MainActivity.kt
+++ app/src/main/java/com/devyk/kotlin_github/MainActivity.kt
@@ -2,11 +2,16 @@ package com.devyk.kotlin_github
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = javaClass.simpleName;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG,"init first init")
     }
 }
